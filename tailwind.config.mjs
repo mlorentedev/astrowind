086ab4e72c44fb3183f009e9ad76a52f8b6cc821
manu <manloralm@outlook.com>
2025-02-07 22:07:18 -0700

    Add landing with copy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,9 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    screens: {
+      'custom': '875px',
+    },
     extend: {
       typography: {
         DEFAULT: {
